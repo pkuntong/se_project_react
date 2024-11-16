08d7acf3ef029a174145c64251dc217da147bbca
Your Name <you@example.com>
2024-11-16 18:31:52 -0500

    update vite.config.js

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter >
       <App />
     </BrowserRouter>
   </React.StrictMode>
