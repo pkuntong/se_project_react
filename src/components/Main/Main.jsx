428170d9e9aff6dd41fa5836173862cbbf30fbef
Your Name <you@example.com>
2024-11-17 13:37:01 -0500

    defaultclothingitems updated

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,6 @@
 import { useContext } from "react";
-import "./Main.css"; 
+
+import "./Main.css";
 import WeatherCard from "../WeatherCard/WeatherCard";
 import ItemCard from "../ItemCard/ItemCard";
 // import { defaultClothingItems } from "../../utils/constants";
@@ -16,24 +17,24 @@ function Main({ weatherData, weatherTemp, onCardClick, clothingItems }) {
         <h1 className="cards__text">
           Today is {temp} &deg; {currentTemperatureUnit} / You may want to wear:
         </h1>
-            <ul className="cards__list">
-              {clothingItems
-                .filter((item) => {
-                  return item.weather === weatherData.type;
-                })
-                .map((item) => {
-                  return (
-                    <ItemCard
-                      key={item._id}
-                      item={item}
-                      onCardClick={onCardClick}
-                    />
+        <ul className="cards__list">
+          {clothingItems
+            .filter((item) => {
+              return item.weather === weatherData.type;
+            })
+            .map((item) => {
+              return (
+                <ItemCard
+                  key={item._id}
+                  item={item}
+                  onCardClick={onCardClick}
+                />
               );
-            })}    
-          </ul>
-        </section>
+            })}
+        </ul>
+      </section>
     </main>
   );
-}     
+}
 
-export default Main; 
+export default Main;
