1fc886e9eca19b02da939786e10bd8fd29d2015e
Your Name <you@example.com>
2024-11-17 12:55:10 -0500

    user name updated

--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -5,9 +5,9 @@ function SideBar() {
   return (
     <div className="sidebar">
       <img className="sidebar__avatar" src={avatar} alt="Default avatar" />
-      <p className="sidebar__username">User name</p>
+      <p className="sidebar__username">Terrence Tegegne</p>
     </div>
   );
 }
 
-export default SideBar;
+export default SideBar; 
